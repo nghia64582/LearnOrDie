4bee80d6ac25cb63b4327099f3b6b0edd11460ac
nghiavt <nghiavt@sandinh.net>
2025-03-20 17:57:07 +0700

    update css

--- Code/Java/draft1.java
+++ Code/Java/draft1.java
@@ -1,5 +1,26 @@
 class Solution {
-    public boolean isScramble(String s1, String s2) {
-        q
+
+    private void dfs(Map<String, PriorityQueue<String>> graph, String from, List<String> result) {
+        PriorityQueue<String> tos = graph.get(from);
+        while (tos != null && !tos.isEmpty()) {
+            String to = tos.poll();
+            dfs(graph, to, result);
+        }
+        result.add(0, from);
+    }
+
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> graph = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            if (!graph.containsKey(from)) {
+                graph.put(from, new PriorityQueue<>());
+            }
+            graph.get(from).add(to);
+        }
+        List<String> result = new ArrayList<>();
+        dfs(graph, "JFK", result);
+        return result;
     }
 }
