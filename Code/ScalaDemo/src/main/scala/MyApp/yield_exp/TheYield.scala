ac99fd4e21477556c0235ed4ad8fd69f2c55a335
nghiavt <nghiavt@sandinh.net>
2025-03-31 18:16:36 +0700

    Update TheYield.scala

--- Code/ScalaDemo/src/main/scala/MyApp/yield_exp/TheYield.scala
+++ Code/ScalaDemo/src/main/scala/MyApp/yield_exp/TheYield.scala
@@ -7,5 +7,16 @@ object TheYield {
     // using yield and for-loop to return new collection from existing list
     var result: List[Int] = for (element <- r) yield element * 2
     println(result)
+
+    // using yield with nested loop like flatMap
+    var nestedLoopResult = for {
+      x <- 1 to 5
+      y <- 1 to 2
+    } yield List(x, y)
+    println(nestedLoopResult)
+
+    // using yield with conditions to filter the value
+    var filterResult = for (i <- 1 to 10 if i % 2 == 0) yield i
+    println(filterResult)
   }
 }
