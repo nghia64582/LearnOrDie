0f20b3a1f29cd6821634685a8477c627b89ec231
nghia64582 <nghia64582@gmail.com>
2025-04-24 01:41:37 +0700

    ,

--- Projects/FinalProject/Code/sh-be/src/main/java/com/example/sh_be/controllers/UserController.java
+++ Projects/FinalProject/Code/sh-be/src/main/java/com/example/sh_be/controllers/UserController.java
@@ -1,12 +1,6 @@
 package com.example.sh_be.controllers;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.example.sh_be.dto.res.UserDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,5 +8,5 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
 public class UserController {
-    
+
 }
