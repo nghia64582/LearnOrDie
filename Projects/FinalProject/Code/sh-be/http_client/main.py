d4b9ce23796fbd1800a591234d403135a202651d
nghia64582 <nghia64582@gmail.com>
2025-05-07 01:24:03 +0700

    remove final project

--- Projects/FinalProject/Code/sh-be/http_client/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-import time
-
-start = time.time()
-
-login_url = "http://localhost:8080/login-oauth?email="
-r = requests.get(login_url, data={"email": "nghia64582@gmail.com"})
-print(r.content.decode("utf-8"))
-print("Total time : " + str(time.time() - start) + " seconds")
--- Projects/FinalProject/Code/sh-be/readme.md
+++
@@ -1,1 +0,0 @@
- - Backend hệ thống
