b93f9dae6924c87a5424aa372fb51c90f693647b
nghiavt <nghiavt@sandinh.net>
2025-05-26 18:34:05 +0700

    update code

--- Code/Python/LinuxScriptAccessor/main.py
+++ Code/Python/LinuxScriptAccessor/main.py
@@ -10,15 +10,17 @@ key_path = "C:/Users/NghiaVT/.ssh/id_rsa"
 LOG_FOLDER = "/mnt/hdd2/f1chanlogs"
 LOG_PREFIX = "smartfox.log"
 
+log_path = LOG_FOLDER + "/" + LOG_PREFIX
 cmd = f'ssh -i {key_path} {user}@{host} "cat {log_path}"'
 # os.system(cmd)
 
-start_date = datetime(2025, 3, 25)
-end_date = datetime(2025, 3, 30)
+start_date = datetime(2025, 5, 24)
+end_date = datetime(2025, 5, 25)
 
 def run_ssh_command(command):
-    ssh_cmd = f"ssh {user}@{host} '{command}'"
-    result = subprocess.run(ssh_cmd, shell=True, capture_output=True, text=True)
+    passphrase = "nghia123456"
+    ssh_cmd = f"ssh {user}@{host} {command}"
+    result = subprocess.run(ssh_cmd, capture_output=True, text=True, encoding='utf-8', shell=True)
     if result.returncode != 0:
         print(f"[!] Error: {result.stderr}")
         return []
@@ -39,9 +41,11 @@ def filter_files_by_date(filenames):
     return filtered
 
 def analyze_log_lines(lines):
+    # 21 May 2025 | 17:11:39,215 | INFO  | application-akka.actor.default-dispatcher-18 | sd.mailBox.MailBoxActor | null | claimMailBoxSuccess: 4480985 7 8,1000000]
+    # process the log lines here
     for line in lines:
-        if "ERROR" in line or "Exception" in line:
-            print(f"❗ {line}")
+        if "claimMailBoxSuccess:" in line:
+            print(line)
 
 def main():
     print("📁 Getting log file list from server...")
@@ -52,11 +56,12 @@ def main():
     filtered = filter_files_by_date(log_files)
     print(f"📅 Filtered files in range: {filtered}")
 
+    all_files_name = " ".join(f"{LOG_FOLDER}/{f}" for f in log_files)
     for file in filtered:
         print(f"\n📖 Reading {file}...")
-        lines = run_ssh_command(f"cat {remote_dir}/{file}")
+        command = f"cat {LOG_FOLDER}/{file}"
+        lines = run_ssh_command(command)
         analyze_log_lines(lines)
-
 
 if __name__ == "__main__":
     main()
